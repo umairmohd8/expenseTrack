0e7786b5e1daedbfbeecbebdcb9dbfcaed880271
umairmohd8 <mohammed08@protonmail.com>
2021-09-13 13:43:38 +0530

    updated

--- expense/lib/main.dart
+++ expense/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expense/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,22 +6,75 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  final List<Transaction> trans = [
+    Transaction("12", "milk", 20.0, DateTime.now()),
+    Transaction("14", "cookies", 43, DateTime.now())
+  ];
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('September'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Card(
-            child: Container(
-              child: Text('Expenses: R1000'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('September'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              height: 50,
+              child: Card(
+                color: Colors.grey,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(' - Expenses'),
+                    Text(' + Income'),
+                  ],
+                ),
+                elevation: 10,
+              ),
             ),
-          ),
-          Card(child: Text('Txn'),)
-        ],
+            Column(
+              children: trans.map((e) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      SizedBox(width: 5,),
+                      Container(
+                        decoration: BoxDecoration(shape: BoxShape.circle),
+                        child: Icon(
+                          Icons.casino,
+                          color: Colors.grey,
+                          size: 25.0,
+                        ),
+                      ),
+                      SizedBox(width: 10,),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(e.title,
+                          style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+                          ),
+                          Text(e.date.toString()),
+                        ],
+                      ),
+                      Spacer(),
+      
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(border: Border.all(), color: Colors.red),
+                        child: Text(e.amt.toString(),
+                        style: TextStyle(color: Colors.black),))
+                        
+                    ],
+                  ),
+                );
+              }).toList(),  //mapped into list of widgets
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ expense/lib/transaction.dart
@@ -0,0 +1,8 @@
+class Transaction {
+  final String id;
+  final String title;
+  final double amt;
+  final DateTime date;
+
+  Transaction(this.id, this.title, this.amt, this.date);
+}
